97010b7fe98d8935f0718ff75fa290d7e8af4281
leoz <956731563@qq.com>
2019-04-23 14:45:18 +0800

    sorcket.io聊天室，总结

---
+++ README.md
@@ -0,0 +1,12 @@
+# websocket_game1
+基于Websocket学习（基础）
+### demo1
+websocket实现消息发送接收
+### demo2,demo3
+websocket简单聊天功能
+### demo4
+websocket简单聊天功能优化，通过JSON转换传输对象数据
+### demo5
+socket.io入门，可以直接发送对象，可以自定义事件。
+### demo6
+socket.io改造聊天功能,socket.on监听客户端，io.emit发送广播
--- demo6/wsServer.js
+++ demo6/wsServer.js
@@ -10,12 +10,14 @@ app.listen(PORT)
 io.on('connection', function(socket){
 	clientCount++
 	socket.nickname = 'user' + clientCount
+	// 客户端进入
 	io.emit('enter', socket.nickname + ' comes in')
-
+	// 接收客户端消息
 	socket.on('message', function(str){
+		// 广播客户端消息
 		io.emit('message', socket.nickname + ' says: ' + str)
 	})
-
+	// 客户端断开
 	socket.on('disconnect', function(){
 		io.emit('leave', socket.nickname + ' left')
 	})
